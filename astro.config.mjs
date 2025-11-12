006e2e430121ae9dcf24efdaaab50f5f2282b580
Arnau Nin <ninbsnss92@gmail.com>
2025-11-12 19:25:47 +0100

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://arnaunin.github.io/',
   base: '/lista_tareas_astro/',
-  integrations: [react()]
+  outDir: 'dist',
+  integrations: [react()],
 });
